d13b7b3a8689226efc4cc828ea913232f778cd75
sharan <shivasharan1989@gmail.com>
2016-06-04 00:21:16 -0700

    hellllo

--- DHC/src/Snipe/com/Apple.java
+++ DHC/src/Snipe/com/Apple.java
@@ -5,7 +5,7 @@ public class Apple {
 	public static void main(String[] args) {
 		// TODO Auto-ge nerated method stub
 		
-		System.out.println("sat noon operation");   
+		System.out.println("sat nooon operation");   
 
 	}
  
