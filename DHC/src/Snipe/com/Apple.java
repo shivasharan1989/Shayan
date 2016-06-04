649bc8745b4b91184a33bf04d4fe91105f49a4a4
Sharan <Sharan@Sharan-PC>
2016-06-03 23:31:59 -0700

    hello

--- DHC/src/Snipe/com/Apple.java
+++ DHC/src/Snipe/com/Apple.java
@@ -5,7 +5,7 @@ public class Apple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("saturdayy operation");
+		System.out.println("saturdayy operation");   
 
 	}
  
